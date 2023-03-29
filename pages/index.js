4b6ded10636e32915b9f67a8ca546648ba0380a8
genaldpaulo <114406756+genaldpaulo@users.noreply.github.com>
2023-03-30 00:01:39 +0800

    update index.js on pages

--- pages/index.js
+++ pages/index.js
@@ -27,8 +27,8 @@ export default function Home({ allPostsData }) {
       </Head>
       <section className={utilStyles.headingMd}>
 
-        <p>Hello, I’m Gen. I’m a software engineer and a translator {' '}
-          (English/Japanese). You can contact me on Twitter.</p>
+        <p>Hello, I’m Gen. I’m a software engineer and I speak {' '}
+          (English/Tagalog/Ilocano). You can contact me on <a href="https://www.instagram.com/genaldpaulo/"></a> Instagram.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
